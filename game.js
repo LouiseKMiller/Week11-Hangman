13acfe75dc9e83b13d05f8507faa4f903807f46f
LouiseKMiller <louise.austex@gmail.com>
2016-07-09 15:41:37 -0500

    add a few words

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 exports.randomWord = {
-	words: ['brush', 'golden lab', 'hot dog', 'avacado', 'portmanteau'],
+	words: ['brush', 'golden lab', 'hot dog', 'avacado', 'portmanteau', "computer", "backpack", "mysql"],
 	pickWord: function(){
 		var random = Math.floor(Math.random()*this.words.length);
 		return this.words[random];
